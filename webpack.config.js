e3ca8d93edee50ad73650bfefd221c6034d09c51
Pankaj Jangid <pankaj@daksh.local>
2024-07-20 09:20:47 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  target: "node",
+  mode: "production", // or 'development' depending on your needs
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: {
+      name: "MyLibrary", // Replace with your library name
+      type: "umd",
+    },
+    globalObject: "this",
+  },
+  optimization: {
+    splitChunks: false, // Disable splitChunks unless necessary
+  },
+  devtool: "source-map",
+  performance: {
+    hints: "warning",
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Clean output directory before build
+  ],
+  externals: {
+    lodash: {
+      commonjs: "lodash",
+      commonjs2: "lodash",
+      amd: "lodash",
+      root: "_",
+    },
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React",
+    },
+  },
+};
