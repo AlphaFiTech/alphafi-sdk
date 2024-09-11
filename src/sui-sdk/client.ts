766a53e71ba365bfcab1d015eaa493a6af949859
Pankaj Jangid <pankaj@alphafi.xyz>
2024-09-11 22:12:31 +0700

    forgot imports

--- src/sui-sdk/client.ts
+++ src/sui-sdk/client.ts
@@ -3,8 +3,9 @@
 /* This file will contain the setup for the Sui Client:
  */
 
-import { SuiClient } from "@mysten/sui/client";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 import { SuiNetwork } from "../common/types";
+import { conf, CONF_ENV } from "../common/constants";
 
 const suiClient = new SuiClient({
   url: getFullnodeUrl(conf[CONF_ENV].SUI_NETWORK as SuiNetwork),
