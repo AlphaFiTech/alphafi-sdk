dff3cc9847f1637ddd2edea7fe51bce4a9ed4019
OctoSauce <mitulgarg62@gmail.com>
2024-09-14 00:31:02 +0700

    getVaultBalancesForActiveUsers filter by poolName fix

--- src/sui-sdk/events/fetchLiquidityChangeEvents.ts
+++ src/sui-sdk/events/fetchLiquidityChangeEvents.ts
@@ -44,7 +44,9 @@ export async function fetchLiquidityChangeEvents(
   });
 
   if (params.poolNames) {
-    liquidityChangeEvents = liquidityChangeEvents.filter(e => (poolIdPoolNameMap[e.pool_id] in params.poolNames!));
+    liquidityChangeEvents = liquidityChangeEvents.filter((e) =>
+      params.poolNames!.includes(poolIdPoolNameMap[e.pool_id]),
+    );
   }
 
   return liquidityChangeEvents;
