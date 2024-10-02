b50f35eb1dfe7e461c445de29a4c26a84775bc88
Pankaj Jangid <pankaj@alphafi.xyz>
2024-10-03 03:14:37 +0700

    changed node url to official sui node url

--- src/sui-sdk/client.ts
+++ src/sui-sdk/client.ts
@@ -3,7 +3,7 @@
 /* This file contains the setup for the Sui Client, implemented as a singleton
  */
 
-import { SuiClient } from "@mysten/sui/client";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 
 //import { NetworkType } from "./types";
 
@@ -11,11 +11,7 @@ import { SuiClient } from "@mysten/sui/client";
 let suiClientInstance: SuiClient | null = null;
 
 export function getSuiNodeUrl(): string {
-  const url =
-    "https://api.shinami.com/node/v1/sui_mainnet_53aeba16fc07edd3c1ec5110550c8189";
-  // const url = getFullnodeUrl("mainnet");
-  //const url = "https://mainnet.suiet.app";
-  //const url = "https://sui-mainnet-endpoint.blockvision.org";
+  const url = getFullnodeUrl("mainnet");
   return url;
 }
 
