cf529bc76952b5713b4b420796d431021286442f
Pankaj Jangid <pankaj@alphafi.xyz>
2024-10-04 03:10:26 +0700

    invalidate suiClientInstance if setSuiNodeUrl is called again.

--- src/sui-sdk/client.ts
+++ src/sui-sdk/client.ts
@@ -5,17 +5,23 @@
 
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 
-//import { NetworkType } from "./types";
-
 // Lazy initialization for the SuiClient instance
 let suiClientInstance: SuiClient | null = null;
 let suiNodeUrl: string | undefined = undefined;
 
+/**
+ * Get the current Sui node URL.
+ * If no URL has been set, it defaults to the mainnet full node URL.
+ */
 export function getSuiNodeUrl(): string {
   suiNodeUrl = suiNodeUrl ? suiNodeUrl : getFullnodeUrl("mainnet");
   return suiNodeUrl;
 }
 
+/**
+ * Get the SuiClient instance.
+ * If a new URL has been set via setSuiNodeUrl, it will create a new instance with the updated URL.
+ */
 export function getSuiClient(): SuiClient {
   if (!suiClientInstance) {
     suiClientInstance = new SuiClient({
@@ -25,6 +31,16 @@ export function getSuiClient(): SuiClient {
   return suiClientInstance;
 }
 
+/**
+ * Set a new Sui node URL.
+ * This will invalidate the current SuiClient instance, ensuring that
+ * the next call to getSuiClient returns a new instance with the updated URL.
+ *
+ * @param rpcNodeUrl - The new RPC URL for the Sui client.
+ */
 export function setSuiNodeUrl(rpcNodeUrl: string) {
-  suiNodeUrl = rpcNodeUrl;
+  if (suiNodeUrl !== rpcNodeUrl) {
+    suiNodeUrl = rpcNodeUrl;
+    suiClientInstance = null; // Invalidate the current instance to allow creating a new one
+  }
 }
