f0dae12f78c88b2f95aa52a9018a9a93fe82b519
Pankaj Jangid <pankaj@alphafi.xyz>
2024-10-03 18:42:58 +0700

    fixed CoinType and CoinName for NAVI-LOOP-SUI-VSUI pool

--- src/sui-sdk/functions/fetchUserVaults.ts
+++ src/sui-sdk/functions/fetchUserVaults.ts
@@ -1,5 +1,15 @@
-import { AlphaFiVault, CoinName, PoolName } from "../..";
-import { coinNameTypeMap, poolCoinPairMap, poolInfo } from "../../common/maps";
+import {
+  AlphaFiVault,
+  DoubleAssetPoolNames,
+  PoolName,
+  SingleAssetPoolNames,
+} from "../..";
+import {
+  coinNameTypeMap,
+  poolCoinMap,
+  poolCoinPairMap,
+  poolInfo,
+} from "../../common/maps";
 import { getReceipts } from "./getReceipts";
 
 export async function fetchUserVaults(
@@ -11,8 +21,11 @@ export async function fetchUserVaults(
     if (receipt.length > 0) {
       const name = receipt[0].content.fields.name;
       let res;
-      if (poolInfo[pool].parentProtocolName === "ALPHAFI") {
-        const coin = "ALPHA" as CoinName;
+      if (
+        poolInfo[pool].parentProtocolName === "ALPHAFI" ||
+        poolInfo[pool].parentProtocolName === "NAVI"
+      ) {
+        const coin = poolCoinMap[pool as SingleAssetPoolNames];
         res = {
           poolId: poolInfo[pool].poolId,
           poolName: pool as PoolName,
@@ -22,10 +35,8 @@ export async function fetchUserVaults(
           coinType: coinNameTypeMap[coin],
         };
       } else if (poolInfo[pool].parentProtocolName === "CETUS") {
-        const coinA =
-          poolCoinPairMap[pool as keyof typeof poolCoinPairMap].coinA;
-        const coinB =
-          poolCoinPairMap[pool as keyof typeof poolCoinPairMap].coinB;
+        const coinA = poolCoinPairMap[pool as DoubleAssetPoolNames].coinA;
+        const coinB = poolCoinPairMap[pool as DoubleAssetPoolNames].coinB;
         res = {
           poolId: poolInfo[pool].poolId,
           poolName: pool as PoolName,
@@ -35,16 +46,6 @@ export async function fetchUserVaults(
           coinTypeA: coinNameTypeMap[coinA],
           coinNameB: coinB,
           coinTypeB: coinNameTypeMap[coinB],
-        };
-      } else if (poolInfo[pool].parentProtocolName === "NAVI") {
-        const coin = pool.replace(/^NAVI-/, "") as CoinName;
-        res = {
-          poolId: poolInfo[pool].poolId,
-          poolName: pool as PoolName,
-          receiptName: name,
-          receiptType: receipt[0].content.type,
-          coinName: coin,
-          coinType: coinNameTypeMap[coin],
         };
       }
       if (res !== undefined) vaultsArr.push(res);
