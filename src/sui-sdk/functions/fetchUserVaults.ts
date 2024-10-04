ae59df656ad32e393ec4bdeacdc196a0570b7344
Pankaj Jangid <pankaj@alphafi.xyz>
2024-10-04 12:50:47 +0700

    use Promise.all and map (async f()) to improve performance

--- src/sui-sdk/functions/fetchUserVaults.ts
+++ src/sui-sdk/functions/fetchUserVaults.ts
@@ -15,41 +15,43 @@ import { getReceipts } from "./getReceipts";
 export async function fetchUserVaults(
   address: string,
 ): Promise<AlphaFiVault[]> {
-  const vaultsArr = [];
-  for (const pool of Object.keys(poolInfo)) {
-    const receipt = await getReceipts(pool, address);
-    if (receipt.length > 0) {
-      const name = receipt[0].content.fields.name;
-      let res;
-      if (
-        poolInfo[pool].parentProtocolName === "ALPHAFI" ||
-        poolInfo[pool].parentProtocolName === "NAVI"
-      ) {
-        const coin = poolCoinMap[pool as SingleAssetPoolNames];
-        res = {
-          poolId: poolInfo[pool].poolId,
-          poolName: pool as PoolName,
-          receiptName: name,
-          receiptType: receipt[0].content.type,
-          coinName: coin,
-          coinType: coinNameTypeMap[coin],
-        };
-      } else if (poolInfo[pool].parentProtocolName === "CETUS") {
-        const coinA = poolCoinPairMap[pool as DoubleAssetPoolNames].coinA;
-        const coinB = poolCoinPairMap[pool as DoubleAssetPoolNames].coinB;
-        res = {
-          poolId: poolInfo[pool].poolId,
-          poolName: pool as PoolName,
-          receiptName: name,
-          receiptType: receipt[0].content.type,
-          coinNameA: coinA,
-          coinTypeA: coinNameTypeMap[coinA],
-          coinNameB: coinB,
-          coinTypeB: coinNameTypeMap[coinB],
-        };
+  const vaultsArr: AlphaFiVault[] = [];
+  await Promise.all(
+    Object.keys(poolInfo).map(async (pool) => {
+      const receipt = await getReceipts(pool, address);
+      if (receipt.length > 0) {
+        const name = receipt[0].content.fields.name;
+        let res: AlphaFiVault | undefined = undefined;
+        if (
+          poolInfo[pool].parentProtocolName === "ALPHAFI" ||
+          poolInfo[pool].parentProtocolName === "NAVI"
+        ) {
+          const coin = poolCoinMap[pool as SingleAssetPoolNames];
+          res = {
+            poolId: poolInfo[pool].poolId,
+            poolName: pool as PoolName,
+            receiptName: name,
+            receiptType: receipt[0].content.type,
+            coinName: coin,
+            coinType: coinNameTypeMap[coin],
+          };
+        } else if (poolInfo[pool].parentProtocolName === "CETUS") {
+          const coinA = poolCoinPairMap[pool as DoubleAssetPoolNames].coinA;
+          const coinB = poolCoinPairMap[pool as DoubleAssetPoolNames].coinB;
+          res = {
+            poolId: poolInfo[pool].poolId,
+            poolName: pool as PoolName,
+            receiptName: name,
+            receiptType: receipt[0].content.type,
+            coinNameA: coinA,
+            coinTypeA: coinNameTypeMap[coinA],
+            coinNameB: coinB,
+            coinTypeB: coinNameTypeMap[coinB],
+          };
+        }
+        if (res) vaultsArr.push(res);
       }
-      if (res !== undefined) vaultsArr.push(res);
-    }
-  }
+    }),
+  );
   return vaultsArr;
 }
