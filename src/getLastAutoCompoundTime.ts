020431f916fb9de64c9941187050a367d3ff49a5
felix11 <khushkothari06@gmail.com>
2024-10-08 23:39:22 +0700

    fixing rebalanceEvent Type

--- src/getLastAutoCompoundTime.ts
+++ src/getLastAutoCompoundTime.ts
@@ -5,7 +5,7 @@ export async function getLastAutoCompoundTime(
   poolName: PoolName,
 ): Promise<string> {
   const endTime = Date.now();
-  const startTime = endTime - 24 * 60 * 60 * 1000; // timestamp for 24 hours ago
+  const startTime = endTime - 60 * 60 * 1000; // timestamp for 24 hours ago
   const events = await fetchAutoCompoundingEvents({
     startTime: startTime,
     endTime: endTime,
