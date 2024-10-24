f7539d430c128816e04716ec6870ecd64179722a
Pankaj Jangid <pankaj@alphafi.xyz>
2024-10-24 16:23:59 +0530

    debug statemetns

--- src/sui-sdk/events/fetchAutoCompoundingEvents.ts
+++ src/sui-sdk/events/fetchAutoCompoundingEvents.ts
@@ -9,14 +9,14 @@ import { fetchEvents } from "./fetchEvents.js";
 import {
   AutoCompoundingEventNode,
   FetchAutoCompoundingEventsParams,
-  RebalanceEventNode,
+  //RebalanceEventNode,
 } from "./types.js";
 
 export async function fetchAutoCompoundingEvents(
   params: FetchAutoCompoundingEventsParams,
 ): Promise<AutoCompoundingEventNode[]> {
   const eventTypesSet = new Set<string>();
-  const rebalanceEventTypesSet = new Set<string>();
+  // const rebalanceEventTypesSet = new Set<string>();
   if (params.poolNames) {
     params.poolNames.forEach((poolName) => {
       const eventType = poolInfo[poolName].autoCompoundingEventType;
@@ -34,25 +34,25 @@ export async function fetchAutoCompoundingEvents(
     });
   }
   // for rebalance
-  if (params.poolNames) {
-    params.poolNames.forEach((poolName) => {
-      const eventType = poolInfo[poolName].rebalanceEventType;
-      if (eventType !== undefined && eventType !== null && eventType !== "") {
-        rebalanceEventTypesSet.add(eventType);
-      }
-    });
-  } else {
-    // Iterate over all the values in poolInfo and add each autoCompoundingEventType to the Set
-    Object.values(poolInfo).forEach((info) => {
-      const eventType = info.rebalanceEventType;
-      if (eventType !== undefined && eventType !== null && eventType !== "") {
-        rebalanceEventTypesSet.add(eventType);
-      }
-    });
-  }
+  // if (params.poolNames) {
+  //   params.poolNames.forEach((poolName) => {
+  //     const eventType = poolInfo[poolName].rebalanceEventType;
+  //     if (eventType !== undefined && eventType !== null && eventType !== "") {
+  //       rebalanceEventTypesSet.add(eventType);
+  //     }
+  //   });
+  // } else {
+  //   // Iterate over all the values in poolInfo and add each autoCompoundingEventType to the Set
+  //   Object.values(poolInfo).forEach((info) => {
+  //     const eventType = info.rebalanceEventType;
+  //     if (eventType !== undefined && eventType !== null && eventType !== "") {
+  //       rebalanceEventTypesSet.add(eventType);
+  //     }
+  //   });
+  // }
 
   const eventTypes = Array.from(eventTypesSet);
-  const rebalanceEventTypes = Array.from(rebalanceEventTypesSet);
+  // const rebalanceEventTypes = Array.from(rebalanceEventTypesSet);
 
   const eventsPromises = eventTypes.map(async (eventType) => {
     const events = (
@@ -72,24 +72,24 @@ export async function fetchAutoCompoundingEvents(
     });
     return events;
   });
-  const rebalanceEventsPromises = rebalanceEventTypes.map(async (eventType) => {
-    const events = (
-      await fetchEvents({
-        startTime: params.startTime,
-        endTime: params.endTime,
-        eventTypes: [eventType],
-      })
-    ).filter((e) => {
-      if (params.poolNames) {
-        return params.poolNames
-          .map((poolName) => poolInfo[poolName].investorId)
-          .includes((e as AutoCompoundingEventNode).investor_id);
-      } else {
-        return true;
-      }
-    });
-    return events;
-  });
+  // const rebalanceEventsPromises = rebalanceEventTypes.map(async (eventType) => {
+  //   const events = (
+  //     await fetchEvents({
+  //       startTime: params.startTime,
+  //       endTime: params.endTime,
+  //       eventTypes: [eventType],
+  //     })
+  //   ).filter((e) => {
+  //     if (params.poolNames) {
+  //       return params.poolNames
+  //         .map((poolName) => poolInfo[poolName].investorId)
+  //         .includes((e as AutoCompoundingEventNode).investor_id);
+  //     } else {
+  //       return true;
+  //     }
+  //   });
+  //   return events;
+  // });
 
   const events = (await Promise.all(eventsPromises)).flat();
 
@@ -97,35 +97,35 @@ export async function fetchAutoCompoundingEvents(
     return e as AutoCompoundingEventNode;
   });
 
-  const rebalanceEvents = (await Promise.all(rebalanceEventsPromises)).flat();
+  // const rebalanceEvents = (await Promise.all(rebalanceEventsPromises)).flat();
 
-  const rebalancingEvents = rebalanceEvents.map((e) => {
-    return e as RebalanceEventNode;
-  });
+  // const rebalancingEvents = rebalanceEvents.map((e) => {
+  //   return e as RebalanceEventNode;
+  // });
 
-  const investorPoolMap = await getInvestorPoolMap();
+  // const investorPoolMap = await getInvestorPoolMap();
 
-  for (const x of autoCompoundingEvents) {
-    if (
-      investorPoolMap.get(x.investor_id) &&
-      poolInfo[investorPoolMap.get(x.investor_id)!].parentProtocolName ==
-        "CETUS"
-    ) {
-      if (
-        "total_amount_a" in x &&
-        x.total_amount_a == 0n &&
-        x.total_amount_b == 0n
-      ) {
-        for (const y of rebalancingEvents) {
-          if (y.investor_id == x.investor_id && y.timestamp == x.timestamp) {
-            // works if its guaranteed that for one timestamp there is only one rebalance for one pool
-            x.total_amount_a = BigInt(y.amount_a_before);
-            x.total_amount_b = BigInt(y.amount_b_before);
-          }
-        }
-      }
-    }
-  }
+  // for (const x of autoCompoundingEvents) {
+  //   if (
+  //     investorPoolMap.get(x.investor_id) &&
+  //     poolInfo[investorPoolMap.get(x.investor_id)!].parentProtocolName ==
+  //       "CETUS"
+  //   ) {
+  //     if (
+  //       "total_amount_a" in x &&
+  //       x.total_amount_a == 0n &&
+  //       x.total_amount_b == 0n
+  //     ) {
+  //       for (const y of rebalancingEvents) {
+  //         if (y.investor_id == x.investor_id && y.timestamp == x.timestamp) {
+  //           // works if its guaranteed that for one timestamp there is only one rebalance for one pool
+  //           x.total_amount_a = BigInt(y.amount_a_before);
+  //           x.total_amount_b = BigInt(y.amount_b_before);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   return autoCompoundingEvents;
 }
@@ -141,9 +141,18 @@ export async function calculateAprForInvestor(
   let previousTimestamp = events[0].timestamp; // Start with the timestamp of the first event
   let previousGrowthRate = 0;
   let previousTimeDiff = 0;
+  let prevCompoundA = 0n;
+  let prevCompoundB = 0n;
 
   const investorPoolMap = await getInvestorPoolMap();
 
+  // const matchInvestor =
+  //   "0x05fa099d1df7b5bfb2e420d5ee2d63508db17c40ce7c4e0ca0305cd5df974e43";
+  // if (events && events.length > 0 && events[0].investor_id === matchInvestor) {
+  //   console.log(
+  //     "Compund A,Total A,Compound B,Total B,Freebalance A,Freebalance B,GrowthA,GrowthB,AvgGrowth,Timestamp,TimeDiff (Min)",
+  //   );
+  // }
   for (const event of events) {
     // Calculate the time difference from the previous event
     let timeDiff = event.timestamp - previousTimestamp; // / (1000 * 60 * 60 * 24);
@@ -153,16 +162,54 @@ export async function calculateAprForInvestor(
     if ("total_amount_a" in event && "total_amount_b" in event) {
       let growthA = 0;
       let growthB = 0;
-      growthA =
-        Number(event.total_amount_a) === 0
-          ? 0
-          : Number(event.compound_amount_a) / Number(event.total_amount_a);
 
-      growthB =
-        Number(event.total_amount_b) == 0
-          ? 0
-          : Number(event.compound_amount_b) / Number(event.total_amount_b);
+      if (Number(event.total_amount_a) === 0) {
+        prevCompoundA += event.compound_amount_a;
+      } else {
+        prevCompoundA = 0n;
+      }
+      if (prevCompoundA > 0n) {
+        growthA =
+          Number(event.total_amount_a) === 0
+            ? 0
+            : Number(event.compound_amount_a + prevCompoundA) /
+              Number(event.total_amount_a - prevCompoundA);
+      } else {
+        growthA =
+          Number(event.total_amount_a) === 0
+            ? 0
+            : Number(event.compound_amount_a) / Number(event.total_amount_a);
+      }
 
+      if (Number(event.total_amount_b) === 0) {
+        prevCompoundB += event.compound_amount_b;
+      } else {
+        prevCompoundB = 0n;
+      }
+
+      if (prevCompoundB > 0n) {
+        growthB =
+          Number(event.total_amount_b) == 0
+            ? 0
+            : Number(event.compound_amount_b + prevCompoundB) /
+              Number(event.total_amount_b - prevCompoundB);
+      } else {
+        growthB =
+          Number(event.total_amount_b) == 0
+            ? 0
+            : Number(event.compound_amount_b) / Number(event.total_amount_b);
+      }
+
+      // if (
+      //   event.investor_id ===
+      //   "0xd060e81548aee885bd3d37ae0caec181185be792bf45412e0d0acccd1e0174e6"
+      // ) {
+      //   console.log(
+      //     timeDiff / (1000 * 60),
+      //     event.timestamp,
+      //     event.timestamp - previousTimestamp,
+      //   );
+      // }
       // if (
       //   event.investor_id ===
       //   "0xd060e81548aee885bd3d37ae0caec181185be792bf45412e0d0acccd1e0174e6"
@@ -177,18 +224,18 @@ export async function calculateAprForInvestor(
       //   );
       // }
       growthRate = (growthA + growthB) / 2; // Averaging growth rates for token A and B
-      // if (
-      //   event.investor_id ===
-      //   "0xd060e81548aee885bd3d37ae0caec181185be792bf45412e0d0acccd1e0174e6"
-      // ) {
+      // if (event.investor_id === matchInvestor) {
       //   console.log(
-      //     `${event.compound_amount_a},${event.total_amount_a},${event.compound_amount_b},${event.total_amount_b},${event.timestamp},${event.free_balance_a},${event.free_balance_b}`,
+      //     `${event.compound_amount_a},${event.total_amount_a},${event.compound_amount_b},${event.total_amount_b},${event.free_balance_a},${event.free_balance_b},${growthA},${growthB},${growthRate},${event.timestamp},${(event.timestamp - previousTimestamp) / (1000 * 60)}`,
       //   );
       // }
       if (Math.abs(growthA - growthB) > 0.5) {
         // skip row, fill with previous event
         growthRate = previousGrowthRate;
         timeDiff = previousTimeDiff;
+      }
+      if (growthRate > 0.005) {
+        growthRate = 0;
       }
     } else if ("total_amount" in event) {
       let compoundAmount: number = Number(event.compound_amount);
