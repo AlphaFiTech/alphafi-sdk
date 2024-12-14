dc18f3eebc57a59803da7c35d391ca305799dd7d
OctoSauce <mitulgarg62@gmail.com>
2024-12-14 14:09:19 +0530

    post build

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1370,7 +1370,8 @@ export const conf = {
 
     HOP_NETWORK: "mainnet",
 
-    TREASURY_ADDRESS: "0x5a9fac4148605191b8e0de25a6671ba8008c344c1558bbaac73a947bd6c903b1",
+    TREASURY_ADDRESS:
+      "0x5a9fac4148605191b8e0de25a6671ba8008c344c1558bbaac73a947bd6c903b1",
 
     DEFUNCT_POOLS: [],
   },
