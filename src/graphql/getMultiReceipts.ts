d2186cc4496a4991c6e8982af20f6156832ed6cb
Pankaj Jangid <pankaj@alphafi.xyz>
2024-12-23 01:30:37 +0530

    split graphql query for multi-receipts

--- src/graphql/getMultiReceipts.ts
+++ src/graphql/getMultiReceipts.ts
@@ -29,46 +29,64 @@ export async function fetchMultiReceipts(
   address: string,
 ): Promise<Map<string, ReceiptGQL[]>> {
   const multiReceipts: any[] = [];
-  let hasNextPage = true;
+
   const receiptTypes = getReceiptTypes();
-  try {
-    while (hasNextPage) {
-      const query = getMultiReceiptsQuery(receiptTypes);
-      const GET_RECEIPTS = gql`
-        ${query}
-      `;
+  const batchSize = 10; // Set the desired batch size
 
-      const result: ApolloQueryResult<any> = await client.query({
-        query: GET_RECEIPTS,
-        variables: {
-          address: address,
-        },
-      });
+  // Convert the receiptTypes object into an array of entries
+  const entries = Object.entries(receiptTypes);
 
-      const { data } = result;
-      const receipts = data.owner;
-      hasNextPage = false;
+  // Create an array to hold the batches
+  const batches: { [k: string]: ReceiptType }[] = [];
+  //const batches = [];
+  // Loop through the entries array and create batches
+  for (let i = 0; i < entries.length; i += batchSize) {
+    const batchEntries = entries.slice(i, i + batchSize);
+    batches.push(Object.fromEntries(batchEntries)); // Convert to object before pushing
+  }
 
-      Object.keys(receipts).forEach((key) => {
-        if (key !== "__typename") {
-          const { pageInfo, nodes } = receipts[key];
-          hasNextPage = hasNextPage || pageInfo.hasNextPage;
-          if (hasNextPage) receiptTypes[key].cursor = pageInfo.endCursor;
-          else receiptTypes[key].cursor = "0";
-          nodes.forEach((node: any) => {
-            multiReceipts.push(node);
-          });
-        }
+  for (const receiptTypes of batches) {
+    let hasNextPage = true;
+
+    try {
+      while (hasNextPage) {
+        const query = getMultiReceiptsQuery(receiptTypes);
+        const GET_RECEIPTS = gql`
+          ${query}
+        `;
+
+        const result: ApolloQueryResult<any> = await client.query({
+          query: GET_RECEIPTS,
+          variables: {
+            address: address,
+          },
+        });
+
+        const { data } = result;
+        const receipts = data.owner;
+        hasNextPage = false;
+
+        Object.keys(receipts).forEach((key) => {
+          if (key !== "__typename") {
+            const { pageInfo, nodes } = receipts[key];
+            hasNextPage = hasNextPage || pageInfo.hasNextPage;
+            if (hasNextPage) receiptTypes[key].cursor = pageInfo.endCursor;
+            else receiptTypes[key].cursor = "0";
+            nodes.forEach((node: any) => {
+              multiReceipts.push(node);
+            });
+          }
+        });
+      }
+    } catch (error) {
+      console.warn(
+        "Error fetching receipts from GraphQL, now fetching from suiClient query",
+      );
+    } finally {
+      Object.keys(receiptTypes).forEach((key) => {
+        receiptTypes[key].cursor = "";
       });
     }
-  } catch (error) {
-    console.warn(
-      "Error fetching receipts from GraphQL, now fetching from suiClient query",
-    );
-  } finally {
-    Object.keys(receiptTypes).forEach((key) => {
-      receiptTypes[key].cursor = "";
-    });
   }
 
   const receiptMap: Map<string, ReceiptGQL[]> = new Map();
