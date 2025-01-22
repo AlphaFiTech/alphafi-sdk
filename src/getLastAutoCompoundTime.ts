d5935eac16a30f268883142bfa984f57e753073c
OctoSauce <mitulgarg62@gmail.com>
2025-01-22 23:27:48 +0400

    faster last autocompound time

--- src/getLastAutoCompoundTime.ts
+++ src/getLastAutoCompoundTime.ts
@@ -5,12 +5,12 @@ export async function getLastAutoCompoundTime(
   poolName: PoolName,
 ): Promise<string> {
   const endTime = Date.now();
-  const startTime = endTime - 24 * 60 * 60 * 1000; // timestamp for 24 hours ago
+  const startTime = endTime - 2 * 60 * 60 * 1000; // timestamp for 2 hours ago
   const events = await fetchAutoCompoundingEvents({
     startTime: startTime,
     endTime: endTime,
     poolNames: [poolName],
   });
   if (events.length > 0) return events[0].timestamp.toString();
-  return "";
+  return startTime.toString();
 }
