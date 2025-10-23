37b74d369df75875e6052bd4de4c5cd262040d34
Pankaj Jangid <pankaj@alphafi.xyz>
2025-10-23 11:08:05 +0530

    formatting

--- src/common/types.ts
+++ src/common/types.ts
@@ -521,16 +521,16 @@ export type Receipt = {
         };
       };
       locked_balance:
-      | {
-        type: string;
-        fields: {
-          head: string;
-          id: { id: string };
-          size: string;
-          tail: string;
-        };
-      }
-      | undefined;
+        | {
+            type: string;
+            fields: {
+              head: string;
+              id: { id: string };
+              size: string;
+              tail: string;
+            };
+          }
+        | undefined;
       name: string;
       owner: string;
       pending_rewards: {
